d12223fd4bb10c202b8cf864799142102bcd16ce
Belyga9000 <vlcode93@gmail.com>
2022-10-19 18:00:09 +0300

    feat: basic server setup and basic pages layout

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+mongoose.connect(
+  "mongodb://localhost:27017/SocialMedia",
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  function (err) {
+    if (err) return console.log(err);
+    app.listen(PORT, function () {
+      console.log("Сервер работает");
+    });
+  }
+);
